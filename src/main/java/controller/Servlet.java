9288b4997cce3ebd3e44de9c5ab4b0392fd7accb
Olena Savinkova <owlindacity@gmail.com>
2019-04-04 12:44:38 +0300

    add Servlet

--- src/main/java/controller/Servlet.java
+++ src/main/java/controller/Servlet.java
@@ -1,9 +1,16 @@
 package controller;
 
 import controller.commands.Command;
-import org.apache.logging.log4j.core.tools.picocli.CommandLine;
+import controller.commands.mainCommand.LogOutCommand;
+import controller.commands.mainCommand.RegistrationCommand;
+import controller.commands.mainCommand.SignInCommand;
+import controller.commands.mainCommand.UnknownCommand;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,5 +18,37 @@ public class Servlet extends HttpServlet {
 
     private Map<String, Command> commands = new HashMap<>();
 
+    public void init() {
+        commands.put("login", new SignInCommand());
+        commands.put("logout", new LogOutCommand());
+        commands.put("registration", new RegistrationCommand());
+        commands.put("exceprion", new UnknownCommand());
+    }
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+        processRequest(request, response);
+    }
 
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+        System.out.println(path);
+        path = path.replaceAll(".*/app/" , "");
+        System.out.println(path);
+        Command command = commands.getOrDefault(path ,
+                (r)->"/index.jsp)");
+        String page = command.execute(request);
+        if(page.contains("redirect")){
+            response.sendRedirect(page.replace("redirect:", "/api"));
+        }else {
+            request.getRequestDispatcher(page).forward(request, response);
+        }
+    }
 }
